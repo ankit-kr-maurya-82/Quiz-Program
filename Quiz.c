6a102cc9293daed27fef39389cb41b7e48e10419
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2025-11-22 22:22:11 +0530

    bug and fix by structure

--- Quiz.c
+++ Quiz.c
@@ -1,31 +1,76 @@
-#include<stdio.h>
-void oneQs();
-char answer();
-void allQs();
-int main(){
+#include <stdio.h>
+
+struct Question
+{
+    char text[200];
+    char a[20], b[20], c[20], d[20];
+    char correct;
+};
+
+int askQuestion(struct Question q);
+
+int main()
+{
+    struct Question quiz[] = {
+        {"1. What is an input device?",
+         "Drum",
+         "Dot Matrix",
+         "LCD",
+         "Joystick",
+         'd'},            
+
+        {"2. What is an output device?",
+         "scanner",
+         "Keyboard",
+         "Plotter",
+         "Joystick",
+         'c'},         
+
+        {"3. Which one is a programming language?",
+         "HTML",
+         "Python",
+         "CSS",
+         "Photoshop",
+         'b'}        
+    };
+
+    int totalQuestions = sizeof(quiz) / sizeof(quiz[0]);
+    int score = 0;
+
+    printf("========== QUIZ START ==========\n\n");
+
+    for (int i = 0; i < totalQuestions; i++) {
+        score += askQuestion(quiz[i]);
+    }
+
+    printf("\n================================\n");
+    printf("Your Score: %d / %d\n", score, totalQuestions);
+    printf("================================\n");
+
     return 0;
 }
 
-void oneQs(){
-    int x;
-    printf("---------- Quiz------------");
-    printf("1. What is input devices?");
-    printf("a) Drum\n");
-    printf("b) Dot matrix\n");
-    printf("c) LCD\n");
-    printf("d) Joystick\n");
-}
+int askQuestion(struct Question q)
+{
+    char answer;
 
-char answer(){
-    char ans;
-    scanf("%c", &ans);
-     if(ans == 'd' || ans == 'D') {
-        printf("\n✔ Correct! Joystick is an input device.\n");
-    } else {
-        printf("\n✘ Wrong answer!\nCorrect answer: d) Joystick\n");
+    printf("%s\n", q.text);
+    printf("a) %s\n", q.a);
+    printf("b) %s\n", q.b);
+    printf("c) %s\n", q.c);
+    printf("d) %s\n", q.d);
+    printf("Your answer: ");
+
+    scanf(" %c", &answer);  // FIXED: ignores previous newline
+
+    if (answer == q.correct || answer == q.correct - 32)
+    {
+        printf(" Correct!\n\n");
+        return 1;
     }
-}
-
-void allQs(){
-    oneQs();
+    else
+    {
+        printf(" Wrong! Correct answer is '%c'\n\n", q.correct);
+        return 0;
+    }
 }
